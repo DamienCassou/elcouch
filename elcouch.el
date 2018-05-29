d9fb6b125ee817b53b515a7d745cf55f994dd982
Damien Cassou <damien@cassou.me>
2018-05-29 09:41:52 +0200

    Add libelcouch to Package-Requires

--- elcouch.el
+++ elcouch.el
@@ -4,7 +4,7 @@
 
 ;; Author: Damien Cassou <damien@cassou.me>
 ;; Url: https://gitlab.petton.fr/DamienCassou/elcouch
-;; Package-requires: ((emacs "25.1") (json-mode "1.0.0"))
+;; Package-requires: ((emacs "25.1") (json-mode "1.0.0") (libelcouch "0.7.0"))
 ;; Version: 0.1.0
 ;; Keywords: data, tools
 
